a46da6a2ac48b4c96d26b78d099aa668ce6742d4
Durgesh Gour <durgesh.gour@mediassistindia.com>
2021-06-16 11:38:49 +0530

    bug fixed

--- src/route.js
+++ src/route.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const fibonacci = (n) => {
     if(n <= 0)
        return [];
-    if (n === 1) {
+    if (n == 1) {
         return [0, 1];
       } else {
         var arr = fibonacci(n - 1);
@@ -12,10 +12,10 @@ const fibonacci = (n) => {
         return arr;
       }
  }
-
 router.post('/fibonacci', (req, res) => {
     var n = req.body.fibNumber;
-    res.render('input',{fib: fibonacci(n), number: n});
+    var arr = fibonacci(n);
+    res.render('input',{fib: arr.length == 0 ? "Invalid Number" : arr.slice(0, -1), number: n});
 })
 
 router.get("/",(req, res) => {
